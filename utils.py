d272057509210072cfe7d8cf8fe61c85ec665910
cicixuxy <cicixuxy@gmail.com>
2025-08-02 10:47:57 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,9 +8,9 @@ def compute_val_metrics(pred_score, true_score):
     srcc_mean = spearmanr(pred_score, true_score)  # SRCC
 
     true_score = np.array(true_score)
-    true_score_lable = np.where(true_score <= 5, 0, 1)
+    true_score_lable = np.where(true_score <= 0.5, 0, 1)
     pred_score = np.array(pred_score)
-    pred_score_lable = np.where(pred_score <= 5, 0, 1)
+    pred_score_lable = np.where(pred_score <= 0.5, 0, 1)
     acc = accuracy_score(true_score_lable, pred_score_lable)
 
     print('acc: {:.3}  srcc_mean: {:.3}  lcc_mean: {:.3}'.format(acc, srcc_mean[0], lcc_mean[0]))
