4551fbfade7cbf17570555b8ed8f5db3b924f27a
cicixuxy <cicixuxy@gmail.com>
2025-08-02 10:42:20 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,29 +3,6 @@ from scipy.stats import pearsonr
 from scipy.stats import spearmanr
 from sklearn.metrics import accuracy_score
 
-def convert_models_to_fp32(model):
-    for p in model.parameters():
-        p.data = p.data.float()
-        if p.grad is not None:
-            p.grad.data = p.grad.data.float()
-
-class AverageMeter(object):
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count  # 计算平均损失
-
-
 def compute_val_metrics(pred_score, true_score):
     lcc_mean = pearsonr(pred_score, true_score)  # PLCC
     srcc_mean = spearmanr(pred_score, true_score)  # SRCC
